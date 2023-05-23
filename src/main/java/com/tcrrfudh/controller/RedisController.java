5f7011d702916f4d3c463abcb693f4345eb2741d
Sad <java_20020916@163.com>
2023-05-23 10:30:28 +0800

    init

---
+++ src/main/java/com/tcrrfudh/controller/RedisController.java
@@ -0,0 +1,13 @@
+package com.tcrrfudh.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tcrrfudh
+ */
+@RestController
+public class RedisController {
+
+
+
+}
